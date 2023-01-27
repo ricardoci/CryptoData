467223d49f6d1cf7d05d5073162901bd1959d6e9
Christian Plasencia <113798773+Chris70A@users.noreply.github.com>
2023-01-26 18:45:08 -0800

    background

--- assets/Toggle.js
+++ assets/Toggle.js
@@ -1,4 +1,4 @@
-var cryptoCardForm = document.querySelector("#cryptoCard");
+var cryptoCardForm = document.querySelector("#cryptoCard"); //
 var weatherCardForm = document.querySelector("#weatherCard");
 
 
@@ -13,3 +13,32 @@ document.querySelector("#weatherButton").addEventListener("click", e => {
     cryptoCardForm.classList.remove("form_hidden");
     weatherCardForm.classList.add("form_hidden");
 });
+
+var currentTime = new Date().getHours();
+var background = document.querySelector(".appWrapper");
+
+
+if (document){
+    console.log(currentTime);
+    if (6 <= currentTime && currentTime < 18) {
+        document.querySelector.appWrapper.style.backgroundColor = "red";
+        // background.classList.remove(".night");
+        // background.classList.add(".day");
+       
+     } else {
+        document.querySelector.appWrapper.style.backgroundColor = "red";
+     }
+}
+
+
+
+
+
+
+
+
+$('#time').text(dayjs().format("h:mm A MM-DD-YY"));
+$('#weathertime').text(dayjs().format("h:mm A MM-DD-YY"));
+
+
+    
